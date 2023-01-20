4ba04857147d43e7a2bafcbd84f85e3a79e7755d
ahmed <aahmedosama165@gmail.com>
2023-01-21 00:22:53 +0200

    update

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Prickles-And-Co'),
+      dir: require('path').join(__dirname, './coverage/My-Portfolio'),
       subdir: '.',
       reporters: [
         { type: 'html' },
